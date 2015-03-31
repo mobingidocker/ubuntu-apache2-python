56cbe44dbb6b4dae8d8d28aca780c3dde8a8996c
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2015-03-31 15:16:09 +0900

    add container status

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "installing" > /var/log/container_status
+
 mkdir -p /srv/django
 cp -r /srv/code /srv/django/app
 chown -R www-data:www-data /srv/django/app
 /usr/bin/supervisord
+
+echo "complete" > /var/log/container_status
