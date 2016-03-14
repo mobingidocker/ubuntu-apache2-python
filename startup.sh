2fe2bc9ed334d7fffc32f546cc3accc13693c730
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-14 17:15:50 +0900

    update scripts Ref: ubuntu-nginx-ruby

--- startup.sh
+++ startup.sh
@@ -2,16 +2,22 @@
 
 echo "installing" > /var/log/container_status
 
+echo "Running init script"
 bash /tmp/init/init.sh
 
 mkdir -p /srv/django
 cp -r /srv/code /srv/django/app
 chown -R www-data:www-data /srv/django/app
 
-pushd /srv/django/app
-	pip install -r requirements.txt
-popd
+echo "Running Pip..."
+cd /srv/django/app
+pip install -r requirements.txt | tee /var/log/pip.log
 
-/usr/bin/supervisord
+if [ -f manage.py ]; then
+  echo "Migrate database"
+  python manage.py migrate | tee /var/log/migrate.log
+fi
 
 echo "complete" > /var/log/container_status
+
+/usr/bin/supervisord
